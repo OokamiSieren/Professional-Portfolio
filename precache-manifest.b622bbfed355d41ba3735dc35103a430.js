268e998bf5529da8a0441e921ebdde3cb98b7ec0
Diana <d_quinlan061006@yahoo.com>
2019-09-16 19:44:44 -0400

    Updates

---
+++ precache-manifest.b622bbfed355d41ba3735dc35103a430.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d582c7d2d210f6d4a1032cae75c7e56",
+    "url": "/OokamiSieren/Professional-Portfolio/index.html"
+  },
+  {
+    "revision": "c9f9ea2cfc57ee1313c6",
+    "url": "/OokamiSieren/Professional-Portfolio/static/css/main.fc2ee8fa.chunk.css"
+  },
+  {
+    "revision": "a44f0aeba47513f417fb",
+    "url": "/OokamiSieren/Professional-Portfolio/static/js/2.7edb9a37.chunk.js"
+  },
+  {
+    "revision": "c9f9ea2cfc57ee1313c6",
+    "url": "/OokamiSieren/Professional-Portfolio/static/js/main.fdfb6bfc.chunk.js"
+  },
+  {
+    "revision": "c05370cf95ad4d5b50b3",
+    "url": "/OokamiSieren/Professional-Portfolio/static/js/runtime~main.6c7d9db3.js"
+  },
+  {
+    "revision": "5a99f45f9958cc7450cf622194dfc1b7",
+    "url": "/OokamiSieren/Professional-Portfolio/static/media/Burger.5a99f45f.PNG"
+  },
+  {
+    "revision": "1663dad9f45d5617459523f24ab34eec",
+    "url": "/OokamiSieren/Professional-Portfolio/static/media/EventizeLogo.1663dad9.PNG"
+  },
+  {
+    "revision": "db6108e52b09c47f6b98e51a6d6236a4",
+    "url": "/OokamiSieren/Professional-Portfolio/static/media/Git-Grub.db6108e5.png"
+  },
+  {
+    "revision": "92de98ac99d4e7d103cb4b53083c67a2",
+    "url": "/OokamiSieren/Professional-Portfolio/static/media/IMG_3021.92de98ac.PNG"
+  },
+  {
+    "revision": "71d986c1ff6bbbbbda098013b2469c10",
+    "url": "/OokamiSieren/Professional-Portfolio/static/media/MDP_1802.71d986c1.jpg"
+  },
+  {
+    "revision": "067d2060c83a291acfc7aa97fcb99101",
+    "url": "/OokamiSieren/Professional-Portfolio/static/media/clicky.067d2060.PNG"
+  },
+  {
+    "revision": "bf017c92a7388d9bcc25846c49009e20",
+    "url": "/OokamiSieren/Professional-Portfolio/static/media/googleBooks.bf017c92.PNG"
+  },
+  {
+    "revision": "0117980469a412f95c5ebc8c5ca32598",
+    "url": "/OokamiSieren/Professional-Portfolio/static/media/tech.01179804.jpg"
+  }
+]);
